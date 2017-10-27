bf53011a443412acaeb02286e1de759acfba834d
GamingWolf <darealgamingwolf@gmail.com>
2017-10-27 19:17:57 +0200

    Updated Assembly info for new release.

--- StellarisModGrabber/Properties/AssemblyInfo.cs
+++ StellarisModGrabber/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StellarisModGrabber")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Stellaris Mod Grabber")]
+[assembly: AssemblyDescription("A simple manager for stellaris mods.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StellarisModGrabber")]
@@ -17,7 +18,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3b62e458-6e92-4ef7-a8bd-46ed2e7708cb")]
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
